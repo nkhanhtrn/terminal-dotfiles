a99e9b2dfa4a67144a09eed816d78e93cd991be0
Ngoc Khanh <nkhanhtran@cogini.com>
2014-01-19 18:47:55 +0700

    remove redundant comments, move to initialize part

--- install.sh
+++ install.sh
@@ -2,33 +2,30 @@
 echo "Start install the configuration:"
 
 ############# Start installer #################
+
+#### Initialize ####
 mkdir $HOME/.vim
 mkdir $HOME/.vim/bundle
+mkdir $HOME/.fonts
 
 #### Oh-My-Zsh ####
 cp -r oh-my-zsh $HOME/.oh-my-zsh
 cp _zshrc $HOME/.zshrc
 chsh -s /bin/zsh
-#### End ####
 
 #### Solarized Colors ####
 sh gnome-terminal-colors/set_dark.sh
 cp -r vim/vim-color-solarized $HOME/.vim/bundle/
-#### End ####
 
 #### Vim ####
 cp -r vim/vundle $HOME/.vim/bundle/
 cp _vimrc $HOME/.vimrc	
 vim +BundleInstall +qall
-#### End ####
 
 #### Powerline ####
 pip install --user powerline
-mkdir $HOME/.fonts
 cp vim/powerline-fonts/*/*.ttf $HOME/.fonts/
 fc-cache -vf $HOME/.fonts/
-#### End ####
 
 #### Git ####
 cp _gitconfig $HOME/.gitconfig
-#### End ####
