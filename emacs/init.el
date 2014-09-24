1e18edf0ad9b1208621cab5e38f684bf45224218
Ngoc Khanh <nkhanhtran@cogini.com>
2014-09-24 10:43:23 +0700

    save autosave files into system temp folder

--- emacs/init.el
+++ emacs/init.el
@@ -2,6 +2,10 @@
 (setq user-mail-address "nkhanhtran@cogini.com")
 (setq-default case-fold-search t)
 (set-language-environment "UTF-8")
+(setq backup-directory-alist
+          `((".*" . ,temporary-file-directory)))
+    (setq auto-save-file-name-transforms
+          `((".*" ,temporary-file-directory t)))
 
 (require 'package)
 (dolist (source '(("marmalade" . "http://marmalade-repo.org/packages/")
