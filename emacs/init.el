7cd66aa2492beab5fc8a07e076bd2f412c18e0c4
Ngoc Khanh <nkhanhtran@cogini.com>
2014-09-25 19:24:42 +0700

    add package auto install, solarized theme

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,5 @@
-
+; ================================================================
+; misc settings
 (setq user-mail-address "nkhanhtran@cogini.com")
 (setq-default case-fold-search t)
 (set-language-environment "UTF-8")
@@ -7,6 +8,9 @@
     (setq auto-save-file-name-transforms
           `((".*" ,temporary-file-directory t)))
 
+
+; ================================================================
+; package manager
 (require 'package)
 (dolist (source '(("marmalade" . "http://marmalade-repo.org/packages/")
                   ("elpa" . "http://tromey.com/elpa/")
@@ -14,3 +18,30 @@
                   ))
   (add-to-list 'package-archives source t))
 (package-initialize)
+
+
+; ================================================================
+; autoload packages at start
+(when (not package-archive-contents)
+  (package-refresh-contents))
+(defvar nkhanhtran/elpa-packages
+    ; evil-mode
+  '(evil
+    ;; Color theme
+    color-theme
+    color-theme-solarized))
+(dolist (p nkhanhtran/elpa-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+
+; ================================================================
+; solarized
+;(load-theme 'solarized-dark t)
+(load-theme 'solarized-dark t)
+
+
+; ================================================================
+; evil-mode
+(require 'evil)
+(evil-mode 1)
