590782072ed11725072e094259f57172b42bff70
Khanh Tran <nkhanh.trn@gmail.com>
2015-03-09 20:57:39 -0300

    move configuration for misc plugins to separate files

--- emacs/init.el
+++ emacs/init.el
@@ -1,33 +1,5 @@
-; ================================================================
-; misc settings
-(setq-default case-fold-search t)
-(set-language-environment "UTF-8")
-(setq backup-directory-alist
-          `((".*" . ,temporary-file-directory)))
-    (setq auto-save-file-name-transforms
-          `((".*" ,temporary-file-directory t)))
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(setq-default indent-tabs-mode nil)
-(show-paren-mode t)
-(setq evil-move-cursor-back nil)
-
-; remember cursor position
-(setq save-place-file "~/.emacs.d/saveplace")
-(setq-default save-place t)
-(require 'saveplace)
-
-; startup setting
-(custom-set-variables
- '(inhibit-startup-screen t)
- '(initial-frame-alist (quote ((fullscreen . maximized)))))
-
 ; load path
 (add-to-list 'load-path "~/.emacs.d/config/")
-
-; ===============================================================
-; adjust some defaults key binding
-(global-set-key (kbd "C-x C-b") 'ibuffer)
 
 ; ================================================================
 ; package manager
@@ -39,8 +11,6 @@
   (add-to-list 'package-archives source t))
 (package-initialize)
 
-
-; ================================================================
 ; autoload packages at start
 (when (not package-archive-contents)
   (package-refresh-contents))
@@ -49,6 +19,10 @@
     ; IDE plugins
     sr-speedbar
     company
+    ; whitespace/indentation
+    clean-aindent-mode
+    dtrt-indent
+    ws-butler
     ; Lisp Mode
     elisp-slime-nav
     ; Web Mode
@@ -81,68 +55,9 @@
 (add-to-list 'custom-theme-load-path custom-theme-directory)
 (load-theme 'nk t)
 
-; ================================================================
-; Lisp Mode
-(progn
-  (require 'elisp-slime-nav)
-  (defun nk-lisp-hook ()
-    (elisp-slime-nav-mode)
-    (turn-on-eldoc-mode)
-    )
-  (add-hook 'emacs-lisp-mode-hook 'nk-lisp-hook)
-)
-
-; ================================================================
-; misc plugins config
-; nyan cat
-(require 'nyan-mode)
-(nyan-mode)
-(nyan-start-animation)
-
-; smooth scrolling
-(require 'smooth-scrolling)
-(setq smooth-scroll-margin 10)
-(setq scroll-conservatively 9999
-      scroll-preserve-screen-position t)
-
-
-; ================================================================
-; custom keyboard shortcuts
-; global keys
-(define-key global-map (kbd "RET") 'newline-and-indent)
-
-
-; ================================================================
-; Web Mode
-(require 'web-mode)
-(web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.xml?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-
-(defun web-mode-hook ()
-  ; indendation
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 4)
-
-  ; auto completion
-  (setq web-mode-ac-sources-alist
-  '(("css" . (ac-source-css-property))
-    ("html" . (ac-source-words-in-buffer ac-source-abbrev)))
-  )
-)
-
-(add-hook 'web-mode-hook 'web-mode-hook)
-
 ;; mode configuration
+(load-library "nk-common")                     ; common settings
+(load-library "nk-misc")                       ; misc plugins
 (load-library "nk-helm")                       ; helm
 (load-library "nk-helm-descbinds")             ; helm-descbinds
 (load-library "nk-helm-gtags")                 ; helm-gtags
