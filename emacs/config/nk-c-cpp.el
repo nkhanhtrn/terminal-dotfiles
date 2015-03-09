dc2dc23c8987fd6df76b466a96ca69f69e433a4d
Khanh Tran <nkhanh.trn@gmail.com>
2015-03-08 23:58:53 -0300

    fix C mode error

--- emacs/config/nk-c-cpp.el
+++ emacs/config/nk-c-cpp.el
@@ -6,10 +6,10 @@
 ; function arguments
 (require 'function-args)
 (fa-config-default)
-(define-key c-mode-map  [(contrl tab)] 'moo-complete)
-(define-key c++-mode-map  [(control tab)] 'moo-complete)
-(define-key c-mode-map (kbd "M-o") 'fa-show)
-(define-key c++-mode-map (kbd "M-o") 'fa-show)
+;(define-key c-mode-map  [(contrl tab)] 'moo-complete)
+;(define-key c++-mode-map  [(control tab)] 'moo-complete)
+;(define-key c-mode-map (kbd "M-o") 'fa-show)
+;(define-key c++-mode-map (kbd "M-o") 'fa-show)
 
 ;; semantic
 (require 'cc-mode)
