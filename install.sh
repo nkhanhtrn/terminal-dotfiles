acd1c4c71e9dd52c8447034170d083c0375352fd
Khanh Tran <nkhanh.trn@gmail.com>
2015-03-11 00:11:09 -0300

    create functions to install configs, install configs on choice

--- install.sh
+++ install.sh
@@ -1,5 +1,39 @@
 #!/bin/sh
 
+# git configuration
+install_git()
+{
+    cp git/_config $HOME/.gitconfig
+    cp git/_ignore $HOME/.gitignore_global
+    git config --global core.excludesfile ~/.gitignore_global
+}
+
+# zsh configuration
+install_zsh()
+{
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh\
+        1> /dev/null 2>> /tmp/dotfiles_error.tmp.$$$
+    cp zsh/_config $HOME/.zshrc
+}
+
+# emacs configuration
+install_emacs()
+{
+    if [ ! -d "$HOME/.emacs.d" ]; then
+        mkdir $HOME/.emacs.d
+    fi
+    cp -r emacs/* $HOME/.emacs.d/
+}
+
+# terminal configuration
+install_terminal()
+{
+    if [ ! -d "$HOME/.config/xfce4/terminal" ]; then
+        mkdir $HOME/.config/xfce4/terminal
+    fi
+    cp terminal/_config $HOME/.config/xfce4/terminal/terminalrc
+}
+
 # radiolist dialog to choose install type
 dialog\
     --title "Dotfiles Installer"\
@@ -24,3 +58,18 @@ fi
 # read selection & remove tmp file
 read result < /tmp/dotfile-installer.tmp.$$$
 rm -f /tmp/dotfile-installer.tmp.$$$
+
+# convert the selection into readable install guide
+case $result in
+    "Basic")
+        install_zsh
+        install_emacs;;
+    "Working")
+        install_zsh
+        install_emacs;;
+    "Personal")
+        install_zsh
+        install_emacs
+        install_git
+        install_terminal;;
+esac
