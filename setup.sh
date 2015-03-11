e54b012b592fb72a7653f679d8cca184655ad81b
Khanh Tran <nkhanh.trn@gmail.com>
2015-03-11 17:03:20 -0300

    add setup menu

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+menu_choice="/tmp/dotfile-menu.tmp.$$$"
+dialog=""
+result=0
+
+# check if whiptail or dialog is install
+read dialog <<< "$(which whiptail dialog 2> /dev/null)"
+if [ -z $dialog ]
+then
+    echo "Whiptail or Dialog not found. Please install one of them first!"
+    exit 1
+fi
+
+# menu dialog
+while [ $result -ne 3 ]
+do
+    $dialog --title "Doftiles Setup"\
+            --backtitle "Terminal Dotfiles"\
+            --menu "Choose one: " 10 40 3\
+            1 "Install"\
+            2 "Update"\
+            3 "Exit"\
+            2> "$menu_choice"
+
+    # read input, rm input tmp, create error tmp
+    read result < "$menu_choice"
+    rm -f "$menu_choice"
+
+    # convert the selection into readable install guide
+    case $result in
+        1)
+            /bin/bash install.sh;;
+        2)
+            /bin/bash update.sh;;
+    esac
+done
