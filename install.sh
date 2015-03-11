fdb84a623e8a8738e3318a10752a228ee5a737a6
Khanh Tran <nkhanh.trn@gmail.com>
2015-03-10 23:50:52 -0300

    remove old installation code, add dialog for selection

--- install.sh
+++ install.sh
@@ -1,44 +1,26 @@
 #!/bin/sh
-echo "Start install the configuration"
 
-############# Start installer #################
+# radiolist dialog to choose install type
+dialog\
+    --title "Dotfiles Installer"\
+    --backtitle "Dotfiles Installer"\
+    --radiolist "\nChoose the desire type of installation\n" 12 40 3\
+    "Basic" "zsh + emacs" on\
+    "Working" "zsh + emacs + git" off\
+    "Personal" "All configurations" off\
+    2> /tmp/dotfile-installer.tmp.$$$
 
-#### Initialize ####
-if [ ! -d "$HOME/.config/xfce4/terminal" ]; then
-    mkdir $HOME/.config/xfce4/terminal
+# exit if don't select anything in dialog
+if [ $? -ne 0 ] 
+then
+    dialog\
+        --title "Dotfiles Installer"\
+        --backtitle "Dotfiles Installer"\
+        --msgbox "\nInstallation Cancel!" 7 25
+    clear
+    exit 0
 fi
 
-# emacs folder preparation
-if [ ! -d "$HOME/.emacs.d" ]; then
-    mkdir $HOME/.emacs.d
-fi
-
-#### Emacs ####
-echo "======= Emacs Setup ======"
-cp -r emacs/* $HOME/.emacs.d/
-echo "==========================\n"
-
-#### Nvm ####
-echo "======= NVM Setup ======="
-git clone https://github.com/creationix/nvm.git ~/.nvm
-echo "===============================\n"
-
-#### Terminal Configuration ####
-echo "======= XFCE4 Terminal setup ======="
-cp terminal/_config $HOME/.config/xfce4/terminal/terminalrc
-echo "==============================\n"
-
-#### Git ####
-echo "======= Git setup ======="
-cp git/_config $HOME/.gitconfig
-cp git/_ignore $HOME/.gitignore_global
-git config --global core.excludesfile ~/.gitignore_global
-echo "=========================\n"
-
-#### Oh-My-Zsh ####
-echo "======= ZSH Setup ======="
-chsh -s /bin/zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp zsh/_config $HOME/.zshrc
-echo "=========================\n"
-
+# read selection & remove tmp file
+read result < /tmp/dotfile-installer.tmp.$$$
+rm -f /tmp/dotfile-installer.tmp.$$$
