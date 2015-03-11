d66d46553246ae359e48bdf511ec7fa7cb7e1d1c
Khanh Tran <nkhanh.trn@gmail.com>
2015-03-11 11:28:09 -0300

    remove ugly output redirect using variables

--- install.sh
+++ install.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
+output='/tmp/dotfiles_error.tmp.$$$'
+select='/tmp/dotfiles_install.tmp.$$$'
+error='dotfiles_error'
 
 ####################### Installation Function ##########################
 # git configuration
 install_git()
 {
-    cp git/_config $HOME/.gitconfig\
-       1> /dev/null 2>> /tmp/dotfiles_error.tmp.$$$\
-        && cp git/_ignore $HOME/.gitignore_global\
-              1> /dev/null 2>> /tmp/dotfiles_error.tmp.$$$\
-        && git config --global core.excludesfile ~/.gitignore_global\
-               1> /dev/null 2>> /tmp/dotfiles_error.tmp.$$$
-       return $?
+    cp git/_config $HOME/.gitconfig 2>> "$output"\
+        && cp git/_ignore $HOME/.gitignore_global 2>> "$output"\
+        && git config --global core.excludesfile ~/.gitignore_global 2>> "$output"
+    return $?
 }
 
 # zsh configuration
 install_zsh()
 {
-    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh\
-        1> /dev/null 2>> /tmp/dotfiles_error.tmp.$$$\
-        && cp zsh/_config $HOME/.zshrc\
-              1> /dev/null 2>> /tmp/dotfiles_error.tmp.$$$
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh 2>> "$output"\
+        && cp zsh/_config $HOME/.zshrc 2>> "$output"
     return $?
 }
 
@@ -28,12 +26,10 @@ install_emacs()
 {
     if [ ! -d $HOME/.emacs.d ]
     then
-        mkdir $HOME/.emacs.d\
-              1> /dev/null 2>> /tmp/dotfiles_error.tmp.$$$
+        mkdir $HOME/.emacs.d 2>> "$output"
     fi
 
-    cp -r emacs/* $HOME/.emacs.d\
-          1> /dev/null 2>> /tmp/dotfiles_error.tmp.$$$
+    cp -r emacs/* $HOME/.emacs.d 2>> "$output"
     return $?
 }
 
@@ -42,87 +38,77 @@ install_terminal()
 {
     if [ ! -d $HOME/.config/xfce4/terminal ]
     then
-        mkdir -p $HOME/.config/xfce4/terminal\
-                 1> /dev/null 2>> /tmp/dotfiles_error.tmp.$$$
+        mkdir -p $HOME/.config/xfce4/terminal 2>> "$output"
     fi
 
-    cp terminal/_config $HOME/.config/xfce4/terminal/terminalrc\
-          1> /dev/null 2>> /tmp/dotfiles_error.tmp.$$$
+    cp terminal/_config $HOME/.config/xfce4/terminal/terminalrc 2>> "$output"
     return $?
 }
 
 
 ########################### Main Function ############################
+
+# check if whiptail or dialog is install
+read dialog <<< "$(which whiptail dialog 2> /dev/null)"
+
 # radiolist dialog to choose install type
-dialog\
+$dialog\
     --title "Dotfiles Installer"\
     --backtitle "Dotfiles Installer"\
     --radiolist "\nChoose the desire type of installation\n" 12 40 3\
     "Basic" "zsh + emacs" on\
     "Working" "zsh + emacs + git" off\
     "Personal" "All configurations" off\
-    2> /tmp/dotfile-installer.tmp.$$$
+    2> "$select"
 
 # exit if don't select anything in dialog
 if [ $? -ne 0 ] 
 then
-    dialog\
+    $dialog\
         --title "Dotfiles Installer"\
         --backtitle "Dotfiles Installer"\
         --msgbox "\nInstallation Cancel!" 7 25
     clear
-    exit 0
 fi
 
 # read input, rm input tmp, create error tmp
-read result < /tmp/dotfile-installer.tmp.$$$
-rm -f /tmp/dotfile-installer.tmp.$$$
-rm dotfiles_error 2> /dev/null
-date > /tmp/dotfiles_error.tmp.$$$
-echo "===============================" >> /tmp/dotfiles_error.tmp.$$$
+read result < "$select"
+rm -f "$select"
+rm -f "$error" 2> /dev/null
+date > "$output"
+echo "===============================" >> "$output"
 
 # convert the selection into readable install guide
 case $result in
     "Basic")
-        install_zsh && install_emacs
-        if [ $? -eq 0 ]
-        then
-            rm /tmp/dotfiles_error.tmp.$$$
-        else
-            cp /tmp/dotfiles_error.tmp.$$$ dotfiles_error
-        fi;;
+        echo "Basic"
+        install_zsh && install_emacs;;
     "Working")
-        install_zsh && install_emacs
-        if [ $? -eq 0 ]
-        then
-            rm /tmp/dotfiles_error.tmp.$$$
-        else
-            cp /tmp/dotfiles_error.tmp.$$$ dotfiles_error
-        fi;;
+        install_zsh && install_emacs;;
     "Personal")
-        install_zsh && install_emacs && install_git && install_terminal
-        if [ $? -eq 0 ]
-        then
-            rm /tmp/dotfiles_error.tmp.$$$
-        else
-            cp /tmp/dotfiles_error.tmp.$$$ dotfiles_error
-        fi;;
+        install_zsh && install_emacs && install_git && install_terminal;;
 esac
 
-# print error msg box when dotfiles_error exists
-if [ -f dotfiles_error ]
+# create error log file if install fail
+if [ $? -eq 0 ]
 then
-    dialog\
-        --title "Dotfiles Installer"\
+    rm "$output"
+else
+    cp "$output" "$error"
+fi
+
+# print error msg box when dotfiles_error exists
+if [ -f $error ]
+then
+    $dialog\
+        --title "Install Failed"\
         --backtitle "Dotfiles Installer"\
         --msgbox "\nInstallation Failed! Consult dotfiles_error for more information." 10 35
     clear
-    exit 0
 else
-    dialog\
-        --title "Dotfiles Installer"\
+    $dialog\
+        --title "Install Success"\
         --backtitle "Dotfiles Installer"\
         --msgbox "\nInstallation Success!" 7 25
     clear
-    exit 1
 fi
