b83d93e55800308f9dccda8003eac708c5edf790
Khanh Tran <nkhanh.trn@gmail.com>
2015-03-19 19:06:44 -0300

    adjust setting for webmode

--- emacs/config/nk-webmode.el
+++ emacs/config/nk-webmode.el
@@ -8,14 +8,9 @@
 (require 'web-mode)
 
 ; apply mode for these extensions
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.json" . web-mode))
 
 ; indentation
 (setq web-mode-markup-indent-offset 2)
