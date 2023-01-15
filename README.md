dba2a0334926f3bbb62785212b0c23b730d86d3a
Khanh Tran <coder@coder.us-central1-a.c.nk-cloud-323802.internal>
2023-01-15 02:31:05 +0000

    add plugins & refactor zsh install code

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # terminal-dotfiles
 
 This is the back-up of my terminal dotfiles.
-
-## How to use
-
-Run `setup-gui.sh` to start the install process.
-The setup file supports UI if `dialog` or `whiptail` is installed.
-Choose between 3 options: basic|work|personal.
-
-Enjoy!
--- install.sh
+++ install.sh
@@ -2,58 +2,47 @@
 result=$1
 
 ####################### Installation Function ##########################
-install_git()
-{
-    echo "Install Git Configuration..."
+install_git () {
+    echo -e "Install Git Configuration..."
     cp git/gitignore_global $HOME/.gitignore_global
     cp git/config $HOME/.gitconfig
-    git config --global core.excludesfile $HOME/.gitignore_global
-    echo ""
 }
 
-install_zsh()
-{
-    echo "Install oh-my-zsh..."
-    git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-    echo "Install ZSH Configuration..."
+clone_git () {
+    if [ ! -d $1 ]; then
+        git clone --recursive $2 $1
+    fi
+}
+
+install_zsh () {
+    echo -e "Install ZSH configuration..."
+    clone_git "$HOME/.oh-my-zsh" "https://github.com/ohmyzsh/ohmyzsh.git"
+    clone_git "$HOME/.oh-my-zsh/plugins/zsh-autosuggestions" "https://github.com/zsh-users/zsh-autosuggestions"
     cp zsh/config $HOME/.zshrc
-    echo "Change to ZSH Shell"
-    chsh -s $(which zsh)
-    echo ""
+    if [ $SHELL != "/bin/zsh" ]; then
+        chsh -s $(which zsh)
+    fi
 }
 
 
-install_vim()
-{
-    echo "Install Vim"
+install_vim () {
+    echo -e "Install Vim..."
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     cp vim/config $HOME/.vimrc
 }
     
 
-install_fonts()
-{
+install_fonts () {
+    echo -e "Install Fonts..."
     cp -r fonts $HOME/.fonts
 }
 
-########################### Main Function ############################
-header="\n=================== INSTALL ========================="
-case $result in
-    "basic")
-        echo -e $header
-        install_zsh && install_vim;;
-    "working")
-        echo -e $header
-        install_zsh && install_vim && install_git;;
-    "personal")
-        echo -e $header
-        install_zsh && install_vim && install_git && install_fonts;;
-    *)
-        echo $"Usage: $0 {basic|working|personal}"
-        exit 1
-esac
+
+echo -e "=================== INSTALL ========================="
+install_zsh && install_vim && install_git && install_fonts
 
 # finishing message
 read -sp "Installation finished. Press ENTER to continue..."
+echo -e ""
 exit 0
